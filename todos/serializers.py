0a327471c5f8a8f94a5d40bdf766cfb7ffbee0c6
Marcelo Surco <marcelo.surco@utec.edu.pe>
2022-12-09 14:02:49 -0500

    Add serializers custom

--- todos/serializers.py
+++ todos/serializers.py
@@ -18,7 +18,7 @@ class TodoSerializer(serializers.ModelSerializer):
 
 
 class TestTodoSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length = 100, allow_blank=True)
+    title = serializers.CharField(max_length = 100)
     body = serializers.CharField()
 
     def validate_title(self, value):
